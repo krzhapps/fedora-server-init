ba9bef89be8cecdf61fb1005b463055dc182fd58
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-04-22 16:03:28 +0200

    add file browser for easy media management

--- jellyfin.sh
+++ jellyfin.sh
@@ -14,4 +14,18 @@ sudo podman run -d \
   -v ~/jellyfin/media:/media:ro,z \
   docker.io/jellyfin/jellyfin:latest
 
+mkdir ~/filebrowser
+touch ~/filebrowser/filebrowser.db
+
+sudo podman run -d \
+  --name media-filebrowser \
+  -p 8080:8080 \
+  --restart=always \
+  -v ~/jellyfin/media:/srv:z \
+  -v ~/filebrowser:/database:Z \
+  docker.io/filebrowser/filebrowser:latest \
+  --port 8080
+
+# sudo podman logs media-filebrowser # Use it to check the randomly generated password
+
 sudo systemctl enable --now podman-restart.service
